aa85cabe41c68171ab9592c49e65a253768d05ac
Angie Xetey <axetey@uci.edu>
2025-10-30 21:07:30 -0700

    Tweet Dates on about page done

--- js/about.js
+++ js/about.js
@@ -12,6 +12,20 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+	
+	// Tweet Dates
+	const dates = tweet_array.map(tweet => tweet.time);
+	const earliestTweetDate = new Date(Math.min(...dates));
+	const latestTweetDate = new Date(Math.max(...dates));
+	
+	const dateFormat = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	document.getElementById('firstDate').innerText = earliestTweetDate.toLocaleDateString(undefined, dateFormat);
+	document.getElementById('lastDate').innerText = latestTweetDate.toLocaleDateString(undefined, dateFormat);
+  
+
+
+
 }
 
 //Wait for the DOM to load
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -10,7 +10,11 @@ class Tweet {
 	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source():string {
         //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+        const text = this.text.toLowerCase();
+        if (text.startsWith("just completed")){
+            return "completed_event";
+        }
+        return "miscellaneous";
     }
 
     //returns a boolean, whether the text includes any content written by the person tweeting.
